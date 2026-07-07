af67974118e9de4ec49af50a1784e6c8567093cb
gtowncity <gtowncity1860@gmail.com>
2026-07-07 07:11:04 +0200

    Expose Windows 98 prototype through query flag

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app/App";
+import { Win98AstroShellPrototype } from "./components/win98";
 import "./styles.css";
+import "./components/win98/win98Prototype.css";
+
+const showPrototype = new URLSearchParams(window.location.search).get("prototype") === "win98";
+const RootComponent = showPrototype ? Win98AstroShellPrototype : App;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RootComponent />
   </React.StrictMode>
 );
-
